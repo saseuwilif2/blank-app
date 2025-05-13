c1abb9e659d0ba774ac214029545467320f3337d
saseuwilif2 <saseuwilif2@gmail.com>
2025-05-13 12:32:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Limbah Hazard Checker", layout="centered")
-st_lottie(lottie_animasi, height=300)
+
 st.title("☣️ Limbah Hazard Checker")
 
 st.markdown("""
